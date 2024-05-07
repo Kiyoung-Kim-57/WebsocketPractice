968cc183180a8bc2a021ab0ac51016d5db5e36f0
Kiyoung <rlaky45@naver.com>
2024-05-07 15:07:17 +0900

    Initial Commit

---
+++ WebsocketProject/WebsocketProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebsocketProject
+//
+//  Created by 김기영 on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebsocketProject/WebsocketProject/WebsocketProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebsocketProjectApp.swift
+//  WebsocketProject
+//
+//  Created by 김기영 on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WebsocketProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
